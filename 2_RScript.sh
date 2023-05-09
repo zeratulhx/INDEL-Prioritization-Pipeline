8da1040eac7e09b03b3617041d43c7b92c060320
Briana Robson <115964372+BrianaCRobson@users.noreply.github.com>
2023-05-10 01:46:21 +1000

    Update 2_RScript.sh

--- 2_RScript.sh
+++ 2_RScript.sh
@@ -2,5 +2,5 @@
 
 module load R/4.2.3
  
-Rscript /stornext/Bioinf/data/lab_davidson/robson.b/Project/Pipeline/AutomatedITD/RCodeforKnownGenesFinalExtraInfo.R
+Rscript set/your/directory/RCodeforKnownGenesFinalExtraInfo.R
  
